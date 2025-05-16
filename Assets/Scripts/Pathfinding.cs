527459b71ebf64a97e0648a6959c206ec4e77b11
Nicolas Belelli <nicolas.belelli@davinci.edu.ar>
2025-05-16 12:56:48 -0300

    pathfinding script

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public List<Node> CalculateBFS(Node start,Node end)
+    {
+        var frontier = new Queue<Node>();
+        frontier.Enqueue(start);
+
+
+        //var reached = new HashSet<Node>();
+        //reached.Add(start);
+
+        var cameFrom = new Dictionary<Node, Node>();
+        cameFrom.Add(start, null);
+        //Hasta aca, tenemos una Queue de nodos, y otro Hashset de nodos. a ambos le agregamos el nodo Start (el primero)
+
+
+        while (frontier.Count > 0)
+        {
+            var current = frontier.Dequeue();
+
+            if(current == end)
+            {
+                var path = new List<Node>();
+                while (current != null)
+                {
+                    path.Add(current);
+                    current = cameFrom[current];
+                }
+                path.Reverse();
+                return path;
+            }
+
+
+
+            foreach (var item in current.Neighbors) //se pregunta para cada vecino del nodo Current
+            {
+                if (!cameFrom.ContainsKey(item))
+                {
+                    frontier.Enqueue(item);
+                    cameFrom.Add(item, current);
+                }
+            }
+
+        }
+
+        return new List<Node>();
+
+    }
+}
